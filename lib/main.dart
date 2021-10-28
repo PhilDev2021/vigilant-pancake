a33690324ad8d8150dfe3cec470a49014ac5be16
CodePhilanthropist <barrigaete@gmail.com>
2021-10-28 20:08:36 +0800

    refactor the code

--- lib/main.dart
+++ lib/main.dart
@@ -55,8 +55,10 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Flexible(
-                child: ListView.builder(
-                    itemBuilder: (_, index) => listDyanmic[index]))
+              child: ListView.builder(
+                itemBuilder: (_, index) => listDyanmic[index],
+              ),
+            )
           ],
         ),
       ),
